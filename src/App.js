34542603377b8bfe7ad1b19c84fc09c975f716b4
nahuellegui12 <nahueljesusleguizamon@gmail.com>
2022-09-15 12:32:16 -0300

    resuelve problemas de ejecucion

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BannerImagen } from "./componentes/Banner";
 import { Header } from "./componentes/Header";
 import { ProductosLista } from "./componentes/Productos/index";
 import { Footer } from "./componentes/Footer";
 import 'boxicons';
-import { ItemCount } from "./componentes/Contador";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ItemCount />
       <BannerImagen />
       <ProductosLista />
       <Footer />
